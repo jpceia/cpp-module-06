e212b75922ddca6ff1b2ef7998a3600969d9630c
João Ceia <joao.p.ceia@gmail.com>
2021-12-23 03:38:08 +0000

    fixed ex02

--- ex02/src/Base.cpp
+++ ex02/src/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 02:35:31 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/22 02:37:09 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/23 03:36:45 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,13 @@
 
 Base* generate(void)
 {
-    int n;
-    
     srand(time(NULL));
-    n = rand() % 3;
+    int n = rand() % 3;
     if (n == 0)
         return (new A);
-    else if (n == 1)
+    if (n == 1)
         return (new B);
-    else
-        return (new C);
+    return (new C);
 }
 
 void identify(Base* p)
@@ -45,12 +42,32 @@ void identify(Base* p)
 
 void identify(Base& p)
 {
-    if (dynamic_cast<A*>(&p))
+    try
+    {
+        A a = dynamic_cast<A&>(p);
         std::cout << "A" << std::endl;
-    else if (dynamic_cast<B*>(&p))
+        return ;
+    }
+    catch (std::exception& e)
+    {
+    }
+    try
+    {
+        B b = dynamic_cast<B&>(p);
         std::cout << "B" << std::endl;
-    else if (dynamic_cast<C*>(&p))
+        return ;
+    }
+    catch (std::exception& e)
+    {
+    }
+    try
+    {
+        C c = dynamic_cast<C&>(p);
         std::cout << "C" << std::endl;
-    else
-        std::cout << "unknown" << std::endl;
+        return ;
+    }
+    catch (std::exception& e)
+    {
+    }
+    std::cout << "unknown" << std::endl;
 }
